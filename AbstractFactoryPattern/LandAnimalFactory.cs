e14a38725c317bc974563bf2747a6978059fdf2c
Jeanette Sonnerborg <jv_88_@hotmail.com>
2022-09-06 21:49:45 +0200

    Step4: Creating Concrete Factory - LandAnimalFactory + SeaAnimalFactory

---
+++ AbstractFactoryPattern/LandAnimalFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace AbstractFactoryPattern
+{
+    public class LandAnimalFactory : AnimalFactory
+    {
+        public override Animal GetAnimal(string AnimalType)
+        {
+            if (AnimalType.Equals("Dog"))
+            {
+                return new Dog();
+            }
+            else if (AnimalType.Equals("Cat"))
+            {
+                return new Cat();
+            }           
+            else
+                return null;
+        }
+    }
+}
---
+++ AbstractFactoryPattern/SeaAnimalFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace AbstractFactoryPattern
+{
+    public class SeaAnimalFactory : AnimalFactory
+    {
+        public override Animal GetAnimal(string AnimalType)
+        {
+            if (AnimalType.Equals("Shark"))
+            {
+                return new Shark();
+            }
+            else
+                return null;
+        }
+    }
+}
