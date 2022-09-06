80f22a1537edf0f97b91ce9e436f1aacf78aa199
Jeanette Sonnerborg <jv_88_@hotmail.com>
2022-09-06 21:53:49 +0200

    Step5: Client - Program

--- AbstractFactoryPattern/Program.cs
+++ AbstractFactoryPattern/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using AbstractFactoryPattern;
+
+Animal animal = null;
+AnimalFactory animalFactory = null;
+string speakSound = null;
+
+animalFactory = AnimalFactory.CreateAnimalFactory("Sea");
+Console.WriteLine("Animal Factory type : " + animalFactory.GetType().Name);
+Console.WriteLine();
+
+
+animal = animalFactory.GetAnimal("Shark");
+Console.WriteLine("Animal Type : " + animal.GetType().Name);
+
+speakSound = animal.speak();
+Console.WriteLine(animal.GetType().Name + " Speak : " + speakSound);
+
+
+
+Console.WriteLine();
+Console.WriteLine("--------------------------");
+
+
+
+animalFactory = AnimalFactory.CreateAnimalFactory("Land");
+Console.WriteLine("Animal Factory type : " + animalFactory.GetType().Name);
+Console.WriteLine();
+
+animal = animalFactory.GetAnimal("Dog");
+Console.WriteLine("Animal Type : " + animal.GetType().Name);
+
+speakSound = animal.speak();
+Console.WriteLine(animal.GetType().Name + " Speak : " + speakSound);
+Console.WriteLine();
+
+
+animal = animalFactory.GetAnimal("Cat");
+Console.WriteLine("Animal Type : " + animal.GetType().Name);
+
+speakSound = animal.speak();
+Console.WriteLine(animal.GetType().Name + " Speak : " + speakSound);
+Console.Read();
+        
